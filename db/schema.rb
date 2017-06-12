a614919340e520d3b0e2ebdaadd546a32eee1d09
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-12 22:59:05 +0200

    model pannels

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170612173735) do
+ActiveRecord::Schema.define(version: 20170612205824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pannels", force: :cascade do |t|
+    t.integer  "publisher_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["publisher_id"], name: "index_pannels_on_publisher_id", using: :btree
+  end
 
   create_table "prints", force: :cascade do |t|
     t.integer  "publisher_id"
@@ -59,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170612173735) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "pannels", "publishers"
   add_foreign_key "prints", "publishers"
   add_foreign_key "radios", "publishers"
   add_foreign_key "televisions", "publishers"
