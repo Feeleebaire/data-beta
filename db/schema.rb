b1b665e8964e5f3447dbfae664334ef116608b9a
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-15 20:51:48 +0200

    target model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615183048) do
+ActiveRecord::Schema.define(version: 20170615185120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,11 @@ ActiveRecord::Schema.define(version: 20170615183048) do
     t.datetime "updated_at", null: false
     t.integer  "ads_id"
     t.index ["ads_id"], name: "index_radios_on_ads_id", using: :btree
+  end
+
+  create_table "targets", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "televisions", force: :cascade do |t|
