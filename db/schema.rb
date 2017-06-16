dda0c9fa64913a9f703db0e61ff034f6aabcc1cc
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-16 19:26:42 +0200

    campaign model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615185120) do
+ActiveRecord::Schema.define(version: 20170616172559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170615185120) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["publisher_id"], name: "index_ads_on_publisher_id", using: :btree
+  end
+
+  create_table "campaigns", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_campaigns_on_user_id", using: :btree
   end
 
   create_table "pannels", force: :cascade do |t|
@@ -87,6 +94,7 @@ ActiveRecord::Schema.define(version: 20170615185120) do
   end
 
   add_foreign_key "ads", "publishers"
+  add_foreign_key "campaigns", "users"
   add_foreign_key "pannels", "ads", column: "ads_id"
   add_foreign_key "prints", "ads", column: "ads_id"
   add_foreign_key "radios", "ads", column: "ads_id"
