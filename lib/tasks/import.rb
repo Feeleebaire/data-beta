ef99a2e4de3e3447394680de3a835616cb8778bb
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-17 19:29:38 +0200

    bulk doesn't work

--- lib/tasks/import.rb
+++
@@ -1,7 +0,0 @@
-  require 'csv'
-
-  csv_text = File.read('/Users/philibert/code/feeleebaire/data-beta/print.csv')
-  csv = CSV.parse(csv_text, :headers => true)
-  csv.each do |row|
-    Publisher.create!(row.to_hash)
-  end
