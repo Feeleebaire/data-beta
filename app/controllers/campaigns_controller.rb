5767d2d14bf8db457e41dd4ad89047bacff537c4
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-18 12:39:14 +0200

    new debut

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -3,13 +3,24 @@ class CampaignsController < ApplicationController
     @campaigns = current_user.campaigns
   end
 
+  def show
+    @campaign = Campaign.find(params[:id])
+  end
+
   def new
+    @campaign = Campaign.new
   end
 
   def create
-
+    @campaign = Campaign.new(campaigns_params)
+    if @campaign.save
+      redirect_to campaign_path(@campaign)
+    else
+      render :new
+    end
   end
 
   def campaigns_params
+    params.require(:campaign).permit(:name, :start_date, :end_date, :user_id)
   end
 end
