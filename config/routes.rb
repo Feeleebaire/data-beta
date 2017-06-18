f2693ea983f9b5653adab9a7c84c41dd87d7142f
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-18 11:22:06 +0200

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-namespace :forest do
-  post '/actions/bulk-import' => 'actions#bulk_import'
-end
+  resources :campaigns
+  namespace :forest do
+    post '/actions/bulk-import' => 'actions#bulk_import'
+  end
 
 end
 
