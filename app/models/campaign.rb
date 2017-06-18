1d715542110d4de452a1d79834a43163d8e6f5d8
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-18 11:13:30 +0200

    validation model campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,8 @@
 class Campaign < ApplicationRecord
+  #associations
   belongs_to :user
+  #validations
+  validates :name, presence: true
+  validates :budget, presence: true
+  validates :start_date, presence: true
 end
