76dc43b60282fc2d3098f23cb17c0be1cd2ed198
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-18 12:19:41 +0200

    controller campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,15 @@
 class CampaignsController < ApplicationController
+  def index
+    @campaigns = current_user.campaigns
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def campaigns_params
+  end
 end
